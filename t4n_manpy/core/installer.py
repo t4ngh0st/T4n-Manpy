ba9e385388f066e5f8332e98eed033aabc73cad9
Lun4rr <darskevoid@gmail.com>
2025-09-18 22:07:10 +0700

    Delete t4n_manpy directory

--- t4n_manpy/core/installer.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-def install_package(package):
-    subprocess.run(["sudo", "xbps-install", "-S", package], check=True)
--- t4n_manpy/utils/xdeb_converter.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-def convert_deb(deb_file):
-    if not os.path.exists(deb_file):
-        raise FileNotFoundError(f"Deb file {deb_file} not found!")
-    subprocess.run(["/usr/bin/t4n/xdeb", deb_file], check=True)
