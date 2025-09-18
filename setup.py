aa6082bfe383714f8b5d86adc149b2fd4256e0d7
Lun4rr <darskevoid@gmail.com>
2025-09-18 22:56:18 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,1 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="t4n-manpy",
-    version="0.1",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "t4n = t4n_manpy.main:main"
-        ]
-    },
-    install_requires=[
-        # Add dependencies if needed
-    ],
-)
+print("Hello World")
